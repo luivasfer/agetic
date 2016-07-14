2fa673796d9a9db7d7f3d038e911eca681d0d46e
Luis Vasquez <luivasfer@gmail.com>
2016-07-14 11:34:29 -0400

    adicionamos los archivos del proyecto

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>AGETIC :: Ministerio de la Presidencia - Bolivia</title>
+
+	<link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+	<link rel="stylesheet" href="css/incio.css">
+	<link rel="stylesheet" href="css/e_hover.css">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+	
+	<div id="fondoColor">
+		<div class="container">
+			<section id="logoPrincipal">
+				<img src="img/svg/logoAgetic.svg">
+			</section>
+			<div id="cajaMenuPrincipal">
+				<section class="menuPrincipal">
+					<a href="#" class="o1 hvr-float">
+						<div class="opciones">
+							<img src="img/svg/nosotros.svg">
+						</div>
+						<h5>AGETIC</h5>
+					</a>
+					<a href="#" class="o2 hvr-float">
+						<div class="opciones">
+							<img src="img/svg/gobierno_electronico.svg">
+						</div>
+						<h5>GOBIERNO ELECTRÓNICO</h5>
+					</a>
+					<a href="#" class="o3 hvr-float">
+						<div class="opciones">
+							<img src="img/svg/comunicacion.svg">
+						</div>
+						<h5>PRENSA</h5>
+					</a>
+					<a href="#" class="o4 hvr-float">
+						<div class="opciones">
+							<img src="img/svg/campana.svg">
+						</div>
+						<h5>CAMPAÑAS</h5>
+					</a>
+					<!-- 
+					<a href="" class="o5">
+						<div class="btTransparencia"></div>
+						<h5>TRANSPARECIA</h5>
+					</a>
+					-->
+				</section>
+			</div>
+		</div>
+	</div>
+
+	<footer>
+		<div class="linea">
+			<div class="banderaBolivia"></div>
+			Estado Plurinacional de Bolivia - Ministerio de la Presidencia
+			<div class="redSocial">
+				<div class="facebook"></div>
+				<div class="twitter"></div>
+			</div>
+		</div>
+
+	</footer>
+	
+	<script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/materialize.js"></script>
+	
+</body>
+</html>
